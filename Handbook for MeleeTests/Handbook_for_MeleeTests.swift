5ce6647c73e2fb4b2726d1544ce51f447cc9d163
Ivan Martinez <thatkawaiiguy@Ivans-Mac.local>
2016-12-11 22:34:15 -0500

    Initial Commit

---
+++ Handbook for MeleeTests/Handbook_for_MeleeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Handbook_for_MeleeTests.swift
+//  Handbook for MeleeTests
+//
+//  Created by Ivan Martinez on 12/11/16.
+//  Copyright © 2016 Ivan Martinez. All rights reserved.
+//
+
+import XCTest
+@testable import Handbook_for_Melee
+
+class Handbook_for_MeleeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
